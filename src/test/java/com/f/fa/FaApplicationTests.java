4dd1262bbddc04a155ab1d1fe1348501314d3474
fei <1623301>
2019-08-02 21:00:55 +0800

    Initial commit

---
+++ src/test/java/com/f/fa/FaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.f.fa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
